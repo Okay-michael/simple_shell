6d4334537cd2eff9121ebafc7ae7ce199e860c3f
Nwosu Friday <fanwosu@gmail.com>
2023-12-12 01:50:49 -0800

    commit cstrcat.c a concatenation program

---
+++ cstrcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cstrcat - this program concatenates two strings
+ * @src: the original string to concatenate
+ * @dest: the other string to be joined to the first
+ * Return: the new string
+ */
+char *cstrcat(char *src, char *dest)
+{
+	char *result = malloc(sizeof(char) * (strlen(src) + strlen(dest) + 1));
+	int i, j;
+
+	for (i = 0, j = 0; ; i++, j++)
+	{
+		if (src[j] == '\0')
+		{
+			j = 0;
+			result[i] = dest[j];
+			i++;
+			j++;
+			for (; dest[j] != '\0'; i++, j++)
+			{
+				result[i] = dest[j];
+			}
+			result[i] = '\0';
+			break;
+		}
+		result[i] = src[j];
+	}
+	return (result);
+}
